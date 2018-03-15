7eae8eda39df713cbadbf1d18600930b630d661f
dinhthienphuc <thienphuc.bka1412@gmail.com>
2018-03-15 07:39:04 +0700

    swap 2 variables without using another temporary variable

--- learn.go
+++ learn.go
@@ -1,42 +1,18 @@
 package main
 
-import "errors"
-
 import "fmt"
 
-func f1(arg int) (int, error) {
-	if 42 == arg {
-		return -1, errors.New("Can't work with 42")
-	}
-
-	return arg + 3, nil
-}
-
-// ArgError initial
-type ArgError struct {
-	arg  int
-	prob string
-}
-
-func (e *ArgError) Error() string {
-	return fmt.Sprintf("%d - %s", e.arg, e.prob)
-}
-
-func f2(arg int) (int, error) {
-	if 42 == arg {
-		return -1, &ArgError{arg, "Can't work with it!"}
-	}
-
-	return arg + 3, nil
-}
-
 func main() {
-	for _, i := range []int{7, 42} {
-		r, e := f1(i)
-		if e != nil {
-			fmt.Println("f1 failed: ", e)
-		} else {
-			fmt.Println("f1 worked: ", r)
-		}
-	}
+	x := 3
+	y := 7
+	fmt.Println(x, y)
+
+	x = x + y
+	fmt.Println(x, y)
+
+	y = x - y
+	fmt.Println(x, y)
+
+	x = x - y
+	fmt.Println(x, y)
 }
